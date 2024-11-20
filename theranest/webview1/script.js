36090cfae5418c2f0306bf6083afed90d0abf1bb
rohith-phoebusai <rohith@phoebusai.com>
2024-11-20 16:00:23 +0530

    basic form

---
+++ theranest/webview1/script.js
@@ -0,0 +1,4 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents form submission for demonstration
+    alert("Form submitted successfully!");
+});
